e634b9304c255469e2cd29852555c83c10d400e9
Sebastian Zaha <sebastian.zaha@gmail.com>
2024-12-11 13:22:54 +0100

    AoC 2024/11

--- advent_of_code/2024/11.js
+++ advent_of_code/2024/11.js
@@ -1,43 +1,40 @@
 const example = `125 17`,
     expectP1 = 55312,
-    expectP2 = 0,
+    expectP2 = 65601038650482,
     resultP1 = 203228,
-    resultP2 = null
+    resultP2 = 240884656550923
 
-function solve(input, runs) {
-    let stones = input.trim().split(' ').map(n => parseInt(n))
+const known = {}
+const rec = (n, turns) => {
+    if (turns === 0) return 1
 
+    const seen = known[n] && known[n][turns]
+    if (seen) return seen
 
-    for (let _i = 0; _i < runs; _i++) {
-        //console.log(_i, stones.length, stones)
-        for (let i = 0; i < stones.length; i++) {
-            if (stones[i] === 0) {
-                stones[i] = 1
-            } else {
-                const len = intLen(stones[i])
-                if (len % 2 === 0) {
-                    const right = stones[i] % Math.pow(10, len/2)
-                    stones[i] = Math.floor(stones[i] / Math.pow(10, len/2))
-                    stones.splice(i+1, 0, right)
-                    i++
-                } else {
-                    stones[i] *= 2024
-                }
-            }
+    if (n === 0) {
+        return rec(1, turns - 1)
+    } else {
+        const len = intLen(n)
+        if (len % 2 === 0) {
+            const left = Math.floor(n / Math.pow(10, len / 2))
+            const right = n % Math.pow(10, len / 2)
+            const l = rec(left, turns - 1)
+            const r = rec(right, turns - 1)
+            known[n] ||= [0]
+            known[n][turns] = l + r
+            return l + r
+        } else {
+            return rec(n * 2024, turns - 1)
         }
     }
-
-    return stones.length
 }
 
 function part1(input) {
-    return solve(input, 25)
+    return input.trim().split(' ').reduce((total, n) => total + rec(parseInt(n), 25), 0)
 }
-
 function part2(input) {
-    return solve(input, 75)
+    return input.trim().split(' ').reduce((total, n) => total + rec(parseInt(n), 75), 0)
 }
 
 import {intLen, run} from '../util.js'
 run("11", example, expectP1, expectP2, part1, resultP1, part2, resultP2)
-
